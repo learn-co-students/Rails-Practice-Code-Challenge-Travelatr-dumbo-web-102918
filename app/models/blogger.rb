ba0bb2f6de7a27a8b82bdd1a938f1241c99d647a
github username <github email address>
2018-12-06 11:24:22 -0500

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,4 +1,7 @@
 class Blogger < ApplicationRecord
  has_many :posts
  has_many :destinations, through: :posts
+ validates :name, uniqueness: true
+ validates :age, numericality: { only_integer: true, greater_than: :0 }
+ validates :bio, length: {mininmum: 30}
 end
